48e6fd62d2d2f0408011e488044b12365acec97f
rifqimaulana201811-ux <rifqimaulana201811@gmail.com>
2025-11-20 16:08:23 +0700

    Add files via upload

---
+++ html 2.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+  <body> 
+    <h1> Kantong Belanja</h1> 
+    <ol type="I">
+      <il>Pisang</il>
+      <il>Jambu</il>
+      <il>Pepaya</il>
+      <il>Melon</il>
+    </ol>  
+     <div>
+      <img src="https://tse1.mm.bing.net/th/id/OIP.Jw38K7gZsNgqZ_1Gsoj0VwHaE8?pid=Api&P=0&h=180" alt="Gambar Pisang">
+      <img src="https://tse3.mm.bing.net/th/id/OIP.dbS3xDaZ4FCsv0s1cFzenQHaGn?pid=Api&P=0&h=180" alt="Gambar Jambu">
+      <img src="https://tse2.mm.bing.net/th/id/OIP.dpSYlx1jbO8ERDjPDBcj1AHaFT?pid=Api&P=0&h=180" alt="Gambar pepaya">
+      <img src="https://tse3.mm.bing.net/th/id/OIP.PyM9OaTNoqWkuIq8fAFLbgHaF7?pid=Api&P=0&h=180" alt="Gambar Melon">
+     </div>      
+  </body>
+</html>
